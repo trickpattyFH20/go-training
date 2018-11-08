6cc30167baa3e307f1e6fbb895d8de59a9187b0a
Patrick Lawler <patricktlawler@gmail.com>
2018-11-08 15:55:11 -0500

    format

--- server.go
+++ server.go
@@ -13,10 +13,15 @@ func newServer() {
 	router.HandleFunc("/getUser/{userID}", func(w http.ResponseWriter, r *http.Request) {
 		userID := mux.Vars(r)["userID"]
 		body := getUser(fmt.Sprintf("https://reqres.in/api/users/%s", userID))
+
 		bodyToJSON(body)
+
 		saveToFile(body)
+
 		fmt.Fprintf(w, string(body))
+
 		w.WriteHeader(http.StatusOK)
+
 	}).Methods("GET", "POST")
 
 	server := negroni.Classic()
