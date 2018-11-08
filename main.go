c5d7138c43a1fc986ba38fea67119ad1c35ce8d4
Patrick Lawler <patricktlawler@gmail.com>
2018-11-08 14:37:30 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	body := get("https://reqres.in/api/users/1")
+
+	bodyToJSON(body)
+
+	saveToFile(body)
+}
+
+func get(url string) []byte {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	return body
+}
+
+func bodyToJSON(body []byte) {
+	var person map[string]interface{}
+	jsonErr := json.Unmarshal(body, &person)
+
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+	fmt.Println(person["data"])
+}
+
+func saveToFile(body []byte) {
+	bodyReader := bytes.NewReader(body)
+	file, err := os.Create("response.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(file, bodyReader)
+}
